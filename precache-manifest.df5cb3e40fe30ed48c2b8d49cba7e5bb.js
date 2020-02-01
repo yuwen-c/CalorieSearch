db367e2f3b939e5e2c9553a6bdcb0080845e2952
yuwen <yuwen.azulejos@gmail.com>
2020-02-01 15:08:44 +0800

    Updates

---
+++ precache-manifest.df5cb3e40fe30ed48c2b8d49cba7e5bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020d2c3e81ec7670e44436d1b89b6e74",
+    "url": "/CalorieSearch/index.html"
+  },
+  {
+    "revision": "fc2256fc78b4bbc92037",
+    "url": "/CalorieSearch/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6eaf3d41bc5501b1695a",
+    "url": "/CalorieSearch/static/css/main.5d1535d4.chunk.css"
+  },
+  {
+    "revision": "fc2256fc78b4bbc92037",
+    "url": "/CalorieSearch/static/js/2.94978ef5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/CalorieSearch/static/js/2.94978ef5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eaf3d41bc5501b1695a",
+    "url": "/CalorieSearch/static/js/main.1141f7d8.chunk.js"
+  },
+  {
+    "revision": "65f83df1527922d218f3",
+    "url": "/CalorieSearch/static/js/runtime-main.f5cd3424.js"
+  }
+]);
